e00e0d15f86e4fee4aa717d1a72f15d8d47332c8
Prefixmodz <166675297+Prefixmodz@users.noreply.github.com>
2025-11-23 05:43:29 +0000

    Initial commit

---
+++ orbis-zycore-c/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+// orbis-zycore-c.pch will be the pre-compiled header
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in stdafx.h
+// and not in this file
+
